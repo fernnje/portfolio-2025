15f204c3f4a9b48ca29e44ff7a8f6c48a58df3bb
Fernn <76133413+fernnje@users.noreply.github.com>
2026-08-18 11:19:03 +0700

    Update BoxDescription.vue

--- src/features/home/components/BoxDescription.vue
+++ src/features/home/components/BoxDescription.vue
@@ -102,7 +102,7 @@ const handleTimelineCreated = (timeline: gsap.core.Timeline, delay: number) => {
           <p class="box-description-details-name">Mongedev</p>
           <div class="box-description-details-location">
             <PinIcon class="box-description-details-location-icon" />
-            <p class="box-description-details-location-copy">{{ t("indonesia") }}</p>
+            <p class="box-description-details-location-copy">{{ t("germany") }}</p>
           </div>
         </div>
         <div class="box-description-line"></div>
